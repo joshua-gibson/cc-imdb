1c812733719185c3dbae46331ee9691478e73099
lawrie-sm <alertrelic@gmail.com>
2019-07-11 14:08:40 +0100

    Added init to models

--- models/casting.rb
+++ models/casting.rb
@@ -1,3 +1,14 @@
+require_relative('../db/sql_runner')
+
 class Casting
+  attr_reader :id, :movie_id, :star_id
+  attr_accessor :fee
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @movie_id = options['movie_id'].to_i
+    @star_id = options['star_id'].to_i
+    @fee = options['id'].to_i
+  end
 
 end
